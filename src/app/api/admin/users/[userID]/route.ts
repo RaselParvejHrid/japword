5dc47a0c53d07ff17ae1296cf46b2bd5ae9b7dc2
RaselParvejHrid <RaselParvejHrid@gmail.com>
2024-12-12 22:04:17 +0600

    /admin/user/[userID] GET Done

--- src/app/api/admin/users/[userID]/route.ts
+++ src/app/api/admin/users/[userID]/route.ts
@@ -3,25 +3,25 @@ import { cloudFirestore } from "@/app/lib/firebase/firebase-admin";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { tutorialID: string } }
+  { params }: { params: { userID: string } }
 ) {
-  const tutorialID = params.tutorialID;
+  const userID = params.userID;
 
   try {
-    let tutorial = await cloudFirestore.doc(`tutorials/${tutorialID}`).get();
+    let user = await cloudFirestore.doc(`tutorials/${userID}`).get();
 
-    if (!tutorial.exists) {
+    if (!user.exists) {
       return NextResponse.json(
-        { message: "No Tutorial with this ID exists." },
+        { message: "No User with this ID exists." },
         { status: 400 }
       );
     }
 
-    return NextResponse.json({ tutorial }, { status: 200 });
+    return NextResponse.json({ user }, { status: 200 });
   } catch (error) {
-    console.error("Error fetching tutorial in Route Handler:", error);
+    console.error("Error fetching user in Route Handler:", error);
     return NextResponse.json(
-      { message: "Failed to fetch tutorial." },
+      { message: "Failed to fetch user." },
       { status: 500 }
     );
   }
