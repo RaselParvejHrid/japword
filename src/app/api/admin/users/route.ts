beb34e6083287025e04e1d7f131f212652a5c9f0
RaselParvejHrid <RaselParvejHrid@gmail.com>
2024-12-12 22:49:55 +0600

    Filtered out own account from users list

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import * as yup from "yup";
 import { cloudFirestore } from "@/app/lib/firebase/firebase-admin";
 
 export async function GET(req: NextRequest) {
   const ownEmail = req.headers.get("X-User-Email");
-  console.log("Own Email", ownEmail);
   try {
-    let users = (await cloudFirestore.collection("users").get()).docs.map(
-      (doc) => ({ id: doc.id, ...doc.data() })
-    );
+    let users = (
+      await cloudFirestore
+        .collection("users")
+        .where("email", "!=", ownEmail)
+        .get()
+    ).docs.map((doc) => ({ id: doc.id, ...doc.data() }));
     return NextResponse.json({ users }, { status: 200 });
   } catch (error) {
     console.error("Error fetching users in Route Handler:", error);
